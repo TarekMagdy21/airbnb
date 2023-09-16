8b1945edab54bf3b5bbfcd01b2b49d21a220a475
tarek magdy <tarekmagdy21@outlook.com>
2023-09-16 20:14:20 +0300

    Added listings to home page and made an action for listings and a listing card

---
+++ app/components/HardButton.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { SafeUser } from "../types";
+
+interface HardButtonProps {
+  currentUser?: SafeUser | null;
+  listingId: string;
+}
+const HardButton: React.FC<HardButtonProps> = ({ currentUser, listingId }) => {
+  const hasFavorited = false;
+  const toggleFavorite = () => {};
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="relative hover:opacity-80 transition cursor-pointer"
+    >
+      <AiOutlineHeart
+        size={28}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={`${hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}`}
+      />
+    </div>
+  );
+};
+
+export default HardButton;
