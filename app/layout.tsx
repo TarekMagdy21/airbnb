55b3e51abca6171da9f9b5e542e18add0b4bef7e
tarek magdy <tarekmagdy21@outlook.com>
2023-09-23 09:42:43 +0300

    SearchModal + filtering

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import ToastProvider from "./providers/ToastProvider";
 import LoginModal from "./components/modals/LoginModal";
 import getCurrentUser from "./actions/getCurrentUser";
 import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -28,6 +29,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToastProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
